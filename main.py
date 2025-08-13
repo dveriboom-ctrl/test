856f5c21967328ea5731da73afb937ef953f80c7
Igor <i.merzlikin@yandex.ru>
2025-08-13 11:15:46 +0300

    Стер все и добавил Hello

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-print('Ураган!')
-print('Привет медвед!')  # This line prints "Hurricane!" in Russian
-print('Третий коммит!')  # This line prints "Hello bear!" in Russian
-print('4 коммит!')  # This line prints "Third commit!" in Russian
+#Все до этого не нужно
+print('Hello, World!')
